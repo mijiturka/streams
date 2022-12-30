4e94a01c1016c882a6637750945f44f5a9f8b85d
Rumyana <mijiturka@tuta.io>
2022-12-30 02:06:23 +0000

    Use a readable HH:MM:SS timestamp

--- update_progress.py
+++ update_progress.py
@@ -1,10 +1,15 @@
+from datetime import timedelta
 from pathlib import Path
 import threading
+
+def readable(seconds):
+    return timedelta(seconds=seconds)
 
 class Progress():
     def __init__(self, movie_length):
         self.now_at = 0
         self.movie_length = movie_length
+        self.movie_length_readable = readable(movie_length)
 
         file_path = "./movie-progress.txt"
         self.f = Path(file_path)
@@ -17,7 +22,11 @@ class Progress():
 
         with self.lock:
             self.now_at += 1
-        self.f.write_text(f'{self.now_at}/{self.movie_length}')
+
+        self.write()
+
+    def write(self):
+        self.f.write_text(f'{readable(self.now_at)}/{self.movie_length_readable}')
 
     def start(self):
         threading.Timer(1.0, self.update).start()
