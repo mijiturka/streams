d7be7abe849a291270eac7ceff3d4115c7afa6ab
Rumyana <mijiturka@tuta.io>
2022-12-30 01:53:35 +0000

    Simple timer writing to a file
    Can be used to display a basic text progress bar in OBS.

---
+++ update_progress.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import threading
+
+class Progress():
+    def __init__(self, movie_length):
+        self.now_at = 0
+        self.movie_length = movie_length
+
+        file_path = "./movie-progress.txt"
+        self.f = Path(file_path)
+
+        self.lock = threading.Lock()
+
+    def update(self):
+        if self.now_at < self.movie_length-1:
+            threading.Timer(1.0, self.update).start()
+
+        with self.lock:
+            self.now_at += 1
+        self.f.write_text(f'{self.now_at}/{self.movie_length}')
+
+    def start(self):
+        threading.Timer(1.0, self.update).start()
+
+progress = Progress(32)
+progress.start()
