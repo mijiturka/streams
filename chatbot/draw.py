e0c02ab86124f820811df6ba35308d6cb8d9a04b
Rumyana <mijiturka@tuta.io>
2023-03-04 21:19:46 +0000

    Limit the number of times a chatter can ask the ai to draw

--- chatbot/draw.py
+++ chatbot/draw.py
@@ -9,8 +9,20 @@ import bot
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+max_wishes = 3
+wishes = {}
+
 def draw(message):
     logging.info(f"{message.sender} asked to draw {message.arguments}")
+    if message.sender not in wishes:
+        wishes[message.sender] = 1
+    else:
+        wishes[message.sender] += 1
+
+    if wishes[message.sender] > max_wishes:
+        logging.warning(f"They've already spent their {max_wishes} wishes")
+        return
+
 
 if __name__ == '__main__':
     logger.debug("starting")
